fa2e05091e0f9621c7863507b1da94c622f4070e
joris villet <julek.krakow@proton.me>
2026-05-21 15:28:05 +0200

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
-import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config";
 
 import playformCompress from "@playform/compress";
 import playformInline from "@playform/inline";
+import svelte from "@astrojs/svelte";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), playformInline(), playformCompress()]
+  integrations: [playformInline(), playformCompress(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  server: {
+    host: "0.0.0.0",
+  },
 });
---
+++ src/components/TestDraggable.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { gsap } from "gsap";
+  import { Draggable } from "gsap/Draggable";
+  import { onMount } from "svelte";
+
+  let elem = $state<HTMLElement>();
+
+  gsap.registerPlugin(Draggable);
+
+  onMount(() => {
+    Draggable.create(elem, {
+      type: "x",
+      onLockAxis: (evt) => console.log(evt),
+    });
+  });
+</script>
+
+<div class="bg-orange-300 h-full w-1/2">
+  <div bind:this={elem} class="w-32 h-32 bg-red-500">Draggable</div>
+</div>
